09733aa1dcb2726251086299148b6a61b61b9a21
Jakub Mościcki <225344@edu.p.lodz.pl>
2023-01-25 15:49:03 +0100

    dodanie widoku nowy projekt

--- App/website/views.py
+++ App/website/views.py
@@ -7,6 +7,21 @@ import json
 views = Blueprint('views', __name__)
 
 
+@views.route('/newproject')
+def new_project():
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added!', category='success')
+    return render_template("newproject.html", user=current_user)
+
+
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
