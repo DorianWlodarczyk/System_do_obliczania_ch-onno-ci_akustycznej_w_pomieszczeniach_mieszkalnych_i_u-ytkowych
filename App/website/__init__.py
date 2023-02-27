70fafa722ca0025d6fdbd4c140e2e4cf6fa92455
Artur229322 <229322@edu.p.lodz.pl>
2023-02-27 01:52:59 +0100

    Small cleanup

--- App/website/__init__.py
+++ App/website/__init__.py
@@ -21,8 +21,7 @@ def create_app():
         app.register_blueprint(auth, url_prefix='/')
 
         from .models import Users
-
-        #create_database(app)
+    
 
         login_manager = LoginManager()
         login_manager.login_view = 'auth.login'
